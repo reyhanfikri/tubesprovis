4ff509f4270d93535a965eecb91670492697f7ce
AgungAnanta <agunganantawijaya@student.upi.edu>
2018-04-18 11:38:05 +0700

    ganti
    ganti i

--- tubesprovis/Controllers/ValuesController.cs
+++ tubesprovis/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace tubesprovis.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Hoeee", "kp" };
+            return new string[] { "ganti", "kp" };
         }
 
         // GET api/values/5
