08fe4bd4e105a1e5ce4a82bc794e32fca929e837
reyhanfikri <yamato.reyhan123@gmail.com>
2018-05-01 16:11:01 +0700

    Nambahin class biasa dan class repository gambar dvd

---
+++ tubesprovis/Model/tb_Gambar_DVD/GambarDVD_Repo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace tubesprovis.Model.tb_Gambar_DVD
+{
+    public class GambarDVD_Repo
+    {
+        private MySqlConnection myConn;
+
+        public GambarDVD_Repo()
+        {
+            string connectionString = "Server=localhost;Database=db_dvd;Username=root;Password=;SslMode=none";
+            myConn = new MySqlConnection(connectionString);
+        }
+
+        public void OpenConnection()
+        {
+            try
+            {
+                myConn.Open();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public void closeConnection()
+        {
+            try
+            {
+                myConn.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public List<GambarDVD_Class> getAllGenre()
+        {
+            try
+            {
+                string query = "SELECT * FROM tb_genre";
+
+                OpenConnection();
+                List<GambarDVD_Class> hasil = myConn.Query<GambarDVD_Class>(query).ToList();
+                closeConnection();
+
+                return hasil;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
