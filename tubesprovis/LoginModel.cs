3ee4cbba006a530d43225691fb7eda1dfea2311a
AgungAnanta <agunganantawijaya@student.upi.edu>
2018-05-02 10:32:39 +0700

    Model Buat Token

---
+++ tubesprovis/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tubesprovis
+{
+    public class LoginModel
+    {
+        private string username;
+        private string password;
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
---
+++ tubesprovis/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tubesprovis
+{
+    public class UserModel
+    {
+        private string name;
+        private string email;
+        private DateTime birthdate;
+
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public DateTime Birthdate { get => birthdate; set => birthdate = value; }
+    }
+}
