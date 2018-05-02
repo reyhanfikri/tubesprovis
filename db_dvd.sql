621aff1459dae58a3ba3134df404ce8e0b74de63
reyhanfikri <yamato.reyhan123@gmail.com>
2018-05-02 13:42:21 +0700

    Update database
    + komentar

--- db_dvd.sql
+++ db_dvd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2018 at 04:46 PM
+-- Generation Time: May 01, 2018 at 04:50 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -27,12 +27,28 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tb_customer` (
-  `id_cust` int(11) NOT NULL,
-  `nama_cust` varchar(100) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `id_cust` int(11) NOT NULL COMMENT 'id primary key untuk tabel tb_customer',
+  `nama_cust` varchar(100) NOT NULL COMMENT 'nama pembeli',
+  `username` varchar(100) NOT NULL COMMENT 'username pembeli yang dapat digunakan untuk log in',
+  `email` varchar(100) NOT NULL COMMENT 'email pembeli yang dapat digunakan untuk log in',
+  `password` varchar(100) NOT NULL COMMENT 'password pembeli yang digunakan untuk log in'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_customer`
+--
+
+INSERT INTO `tb_customer` (`id_cust`, `nama_cust`, `username`, `email`, `password`) VALUES
+(1, 'Ammar A', 'Ammar', 'ammar@gmail.com', 'ammara123'),
+(2, 'IGN Agung AAW', 'Agung', 'ignagung@gmail.com', 'qwerty'),
+(3, 'Reyhan Fikri', 'Reyhan', 'reyhanf@gmail.com', '000001'),
+(4, 'Rifky Syswanto', 'Rifky', 'rifkysyswanto@gmail.com', '0258'),
+(5, 'Andy Batary FN', 'Tary', 'tary@gmail.com', '14789632'),
+(6, 'Winda Mauli', 'winda', 'windamk@gmail.com', 'wndmk'),
+(7, 'Muhammad Budi', 'Budi', 'mbudi@gmail.com', 'budi444'),
+(8, 'Ahmad Asep', 'Ahmad', 'aasep@gmail.com', '11111'),
+(9, 'Siti Hawa', 'siha', 'sihawa@gmail.com', 'sihaaaa'),
+(10, 'Nur Intan', 'Intan', 'nintan@gmail.com', 'intan101');
 
 -- --------------------------------------------------------
 
@@ -41,13 +57,34 @@ CREATE TABLE `tb_customer` (
 --
 
 CREATE TABLE `tb_detail_keranjang` (
-  `id_detail` int(11) NOT NULL,
-  `id_keranjang` int(11) NOT NULL,
-  `id_dvd` int(11) NOT NULL,
-  `qty` int(11) NOT NULL,
-  `harga_jual` int(11) NOT NULL,
-  `total_harga` int(11) NOT NULL
+  `id_detail` int(11) NOT NULL COMMENT 'id primary key untuk tabel tb_detail_keranjang',
+  `id_keranjang` int(11) NOT NULL COMMENT 'menghubungkan data dengan tabel tb_keranjang',
+  `id_dvd` int(11) NOT NULL COMMENT 'menghubungkan data dengan tabel tb_dvd',
+  `qty` int(11) NOT NULL COMMENT 'jumlah dvd yang dibeli',
+  `harga_jual` int(11) NOT NULL COMMENT 'Harga kenyataan yang dijual ke pembeli',
+  `total_harga` int(11) NOT NULL COMMENT 'Total harga dvd yang dibeli'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_detail_keranjang`
+--
+
+INSERT INTO `tb_detail_keranjang` (`id_detail`, `id_keranjang`, `id_dvd`, `qty`, `harga_jual`, `total_harga`) VALUES
+(1, 1, 1, 1, 52000, 52000),
+(2, 2, 2, 1, 50000, 50000),
+(3, 3, 3, 2, 65000, 130000),
+(4, 4, 4, 1, 55000, 55000),
+(5, 5, 10, 3, 55000, 165000),
+(6, 6, 13, 2, 40000, 80000),
+(7, 7, 7, 1, 55000, 55000),
+(8, 8, 17, 2, 63500, 127000),
+(9, 9, 11, 4, 55000, 220000),
+(10, 10, 14, 1, 60000, 60000),
+(11, 11, 8, 1, 60000, 60000),
+(12, 12, 9, 2, 54000, 108000),
+(13, 13, 18, 5, 64500, 322500),
+(14, 14, 20, 1, 52500, 52500),
+(15, 15, 17, 63500, 2, 127000);
 
 -- --------------------------------------------------------
 
@@ -56,11 +93,75 @@ CREATE TABLE `tb_detail_keranjang` (
 --
 
 CREATE TABLE `tb_dvd` (
-  `id_dvd` int(11) NOT NULL,
-  `id_movie` int(11) NOT NULL,
-  `stock` int(11) NOT NULL,
-  `harga_display` int(11) NOT NULL
+  `id_dvd` int(11) NOT NULL COMMENT 'id primary key untuk tabel tb_dvd',
+  `id_movie` int(11) NOT NULL COMMENT 'menghubungkan data dengan tabel tb_movie',
+  `stock` int(11) NOT NULL COMMENT 'Informasi mengenai jumlah stock dvd yang tersedia',
+  `harga_display` int(11) NOT NULL COMMENT 'informasi mengenai harga yang ada di display'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_dvd`
+--
+
+INSERT INTO `tb_dvd` (`id_dvd`, `id_movie`, `stock`, `harga_display`) VALUES
+(1, 1, 10, 52000),
+(2, 2, 12, 50000),
+(3, 3, 5, 65000),
+(4, 4, 5, 55000),
+(5, 5, 15, 66000),
+(6, 6, 8, 62000),
+(7, 7, 10, 55000),
+(8, 8, 5, 60000),
+(9, 9, 7, 54000),
+(10, 10, 2, 80000),
+(11, 11, 5, 60000),
+(12, 12, 3, 75000),
+(13, 13, 6, 40000),
+(14, 14, 10, 60000),
+(15, 15, 8, 65000),
+(16, 16, 4, 40000),
+(17, 17, 6, 63500),
+(18, 18, 15, 64500),
+(19, 19, 4, 53400),
+(20, 20, 8, 52500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_gambar_dvd`
+--
+
+CREATE TABLE `tb_gambar_dvd` (
+  `id_gambar` int(11) NOT NULL COMMENT 'id primary key untuk tabel tb_gambar_dvd',
+  `id_dvd` int(11) NOT NULL COMMENT 'menghubungkan data yang ada di tabel tb_dvd',
+  `url_gambar` varchar(200) NOT NULL COMMENT 'url gambar dvd'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_gambar_dvd`
+--
+
+INSERT INTO `tb_gambar_dvd` (`id_gambar`, `id_dvd`, `url_gambar`) VALUES
+(1, 1, 'https://www.imdb.com/title/tt3501632/mediaviewer/rm1413491712'),
+(2, 2, 'https://www.imdb.com/title/tt0451279/mediaviewer/rm1404907776'),
+(3, 3, 'https://www.imdb.com/title/tt2380307/mediaviewer/rm585455872'),
+(4, 4, 'https://www.imdb.com/title/tt6294822/mediaviewer/rm268720128'),
+(5, 5, 'https://www.imdb.com/title/tt2771200/mediaviewer/rm2049982208'),
+(6, 6, 'https://www.imdb.com/title/tt1396484/mediaviewer/rm2985515264'),
+(7, 7, 'https://www.imdb.com/title/tt0167261/mediaviewer/rm3523884288'),
+(8, 8, 'https://www.imdb.com/title/tt0468569/mediaviewer/rm4023877632'),
+(9, 9, 'https://www.imdb.com/title/tt0264464/mediaviewer/rm3911489536'),
+(10, 10, 'https://www.imdb.com/title/tt0076759/mediaviewer/rm3263717120'),
+(11, 11, 'https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392'),
+(12, 12, 'https://www.imdb.com/title/tt0120338/mediaviewer/rm2647458304'),
+(13, 13, 'https://www.imdb.com/title/tt0266543/mediaviewer/rm943068416'),
+(14, 14, 'https://www.imdb.com/title/tt1301264/mediaviewer/rm1017374464'),
+(15, 15, 'https://www.imdb.com/title/tt2589132/mediaviewer/rm629872896'),
+(16, 16, 'https://www.imdb.com/title/tt1899353/mediaviewer/rm567291648'),
+(17, 17, 'https://www.imdb.com/title/tt2659414/mediaviewer/rm2548715008'),
+(18, 18, 'https://www.imdb.com/title/tt5700672/mediaviewer/rm310842624'),
+(19, 19, 'https://www.imdb.com/title/tt2967578/mediaviewer/rm3145996544'),
+(20, 20, 'https://www.imdb.com/title/tt1028532/mediaviewer/rm560010240');
 
 -- --------------------------------------------------------
 
@@ -69,9 +170,29 @@ CREATE TABLE `tb_dvd` (
 --
 
 CREATE TABLE `tb_genre` (
-  `id_genre` int(11) NOT NULL,
-  `genre` varchar(50) NOT NULL
+  `id_genre` int(11) NOT NULL COMMENT 'id primary key untuk tabel tb_genre',
+  `genre` varchar(50) NOT NULL COMMENT 'genre pada setiap filmnya'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_genre`
+--
+
+INSERT INTO `tb_genre` (`id_genre`, `genre`) VALUES
+(1, 'Action'),
+(2, 'Adventure'),
+(3, 'Comedy'),
+(4, 'Crime'),
+(5, 'Drama'),
+(6, 'Fantasy'),
+(7, 'Historical'),
+(8, 'Horror'),
+(9, 'Mystery'),
+(10, 'Romance'),
+(11, 'Saga'),
+(12, 'Science Fiction'),
+(13, 'Thriller '),
+(14, 'Western');
 
 -- --------------------------------------------------------
 
@@ -80,9 +201,30 @@ CREATE TABLE `tb_genre` (
 --
 
 CREATE TABLE `tb_keranjang` (
-  `id_keranjang` int(11) NOT NULL,
-  `id_cust` int(11) NOT NULL
+  `id_keranjang` int(11) NOT NULL COMMENT 'id primary key untuk tabel tb_keranjang',
+  `id_cust` int(11) NOT NULL COMMENT 'menghubungkan data yang ada di tabel tb_custoomer'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_keranjang`
+--
+
+INSERT INTO `tb_keranjang` (`id_keranjang`, `id_cust`) VALUES
+(1, 1),
+(2, 1),
+(3, 2),
+(4, 3),
+(5, 4),
+(6, 4),
+(7, 5),
+(8, 6),
+(9, 7),
+(10, 7),
+(11, 8),
+(12, 9),
+(13, 9),
+(14, 10),
+(15, 10);
 
 -- --------------------------------------------------------
 
@@ -91,16 +233,42 @@ CREATE TABLE `tb_keranjang` (
 --
 
 CREATE TABLE `tb_movie` (
-  `id_movie` int(11) NOT NULL,
-  `id_genre` int(11) NOT NULL,
-  `judul` varchar(150) NOT NULL,
-  `tahun_produksi` int(4) NOT NULL,
-  `durasi` time NOT NULL,
-  `rating_imdb` int(11) NOT NULL,
-  `rated` varchar(10) NOT NULL,
-  `bahasa` varchar(50) NOT NULL,
-  `subtitle` varchar(50) NOT NULL
+  `id_movie` int(11) NOT NULL COMMENT 'id primary key untuk tabel tb_movie',
+  `id_genre` int(11) NOT NULL COMMENT 'menghubungkana data yang ada di tabel tb_genre',
+  `judul` varchar(150) NOT NULL COMMENT 'judul pada setiap filmnya',
+  `tahun_produksi` int(4) NOT NULL COMMENT 'tahun film tersebut tayang ',
+  `durasi` int(11) NOT NULL COMMENT 'lama film tayang (dalam menit)',
+  `rating_imdb` decimal(10,1) NOT NULL COMMENT 'rating yang diambil dari imdb',
+  `rated` varchar(10) NOT NULL COMMENT 'menunjukan film tersebut layak ditonton untuk kalangan usia berapa saja',
+  `bahasa` varchar(50) NOT NULL COMMENT 'bahasa yang digunakan di film',
+  `subtitle` varchar(50) NOT NULL COMMENT 'subtitle yang tersedia dalam bahasa apa saja'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_movie`
+--
+
+INSERT INTO `tb_movie` (`id_movie`, `id_genre`, `judul`, `tahun_produksi`, `durasi`, `rating_imdb`, `rated`, `bahasa`, `subtitle`) VALUES
+(1, 1, 'Thor: Ragnarok', 2017, 130, '7.9', 'PG-13', 'English', 'Indonesia'),
+(2, 1, 'Wonder Woman', 2017, 141, '7.5', 'PG-13', 'English', 'Indonesia'),
+(3, 3, 'Coco', 2017, 105, '8.5', 'PG', 'English', 'Indonesia'),
+(4, 7, 'The Post', 2017, 116, '7.2', 'PG-13', 'English', 'Indonesia'),
+(5, 10, 'Beauty and the Beast', 2017, 129, '7.2', 'PG', 'English', 'Indonesia'),
+(6, 8, 'It', 2017, 135, '7.5', 'R', 'English', 'English'),
+(7, 2, 'The Lord of the Rings: The Two Towers', 2002, 179, '8.7', 'PG-13', 'English', 'English'),
+(8, 4, 'The Dark Knight', 2008, 152, '9.0', 'PG-13', 'English', 'Indonesia'),
+(9, 4, 'Catch Me If You Can', 2002, 141, '8.1', 'PG-13', 'English', 'English'),
+(10, 6, 'Star Wars: Episode IV - A New Hope', 1977, 121, '8.6', 'PG-13', 'English', 'Indonesia'),
+(11, 1, 'Inception', 2010, 148, '8.8', 'PG-13', 'English', 'Indonesia'),
+(12, 10, 'Titanic', 1997, 194, '7.8', 'PG-13', 'English', 'Indonesia'),
+(13, 2, 'Finding Nemo', 2003, 100, '8.1', 'G', 'English ', 'Indonesia'),
+(14, 5, 'Laskar Pelangi', 2008, 124, '7.8', 'PG-13', 'Indonesia', ''),
+(15, 5, 'Habibie & Ainun', 2012, 120, '7.6', 'PG-13', 'Indonesia', ''),
+(16, 1, 'The Raid: Redemption', 2011, 101, '7.6', 'R', 'Indonesia', 'English'),
+(17, 5, 'Miracle in Cell No. 7', 2013, 127, '8.2', 'PG-13', 'Korea', 'Indonesia'),
+(18, 13, 'Train to Busan', 2016, 118, '7.5', 'PG-13', 'Korea', 'English'),
+(19, 5, 'Secretly, Greatly', 2013, 124, '6.9', 'R', 'Korea', 'English'),
+(20, 5, 'Hachi: A Dog\'s Tale', 2009, 99, '8.1', 'G', 'Japan', 'English');
 
 --
 -- Indexes for dumped tables
@@ -126,6 +294,12 @@ ALTER TABLE `tb_detail_keranjang`
 ALTER TABLE `tb_dvd`
   ADD PRIMARY KEY (`id_dvd`),
   ADD KEY `id_movie` (`id_movie`);
+
+--
+-- Indexes for table `tb_gambar_dvd`
+--
+ALTER TABLE `tb_gambar_dvd`
+  ADD PRIMARY KEY (`id_gambar`);
 
 --
 -- Indexes for table `tb_genre`
@@ -155,32 +329,37 @@ ALTER TABLE `tb_movie`
 -- AUTO_INCREMENT for table `tb_customer`
 --
 ALTER TABLE `tb_customer`
-  MODIFY `id_cust` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cust` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id primary key untuk tabel tb_customer', AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `tb_detail_keranjang`
 --
 ALTER TABLE `tb_detail_keranjang`
-  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id primary key untuk tabel tb_detail_keranjang', AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `tb_dvd`
 --
 ALTER TABLE `tb_dvd`
-  MODIFY `id_dvd` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_dvd` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id primary key untuk tabel tb_dvd', AUTO_INCREMENT=21;
+--
+-- AUTO_INCREMENT for table `tb_gambar_dvd`
+--
+ALTER TABLE `tb_gambar_dvd`
+  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id primary key untuk tabel tb_gambar_dvd', AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT for table `tb_genre`
 --
 ALTER TABLE `tb_genre`
-  MODIFY `id_genre` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_genre` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id primary key untuk tabel tb_genre', AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `tb_keranjang`
 --
 ALTER TABLE `tb_keranjang`
-  MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id primary key untuk tabel tb_keranjang', AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `tb_movie`
 --
 ALTER TABLE `tb_movie`
-  MODIFY `id_movie` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_movie` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id primary key untuk tabel tb_movie', AUTO_INCREMENT=21;
 --
 -- Constraints for dumped tables
 --
