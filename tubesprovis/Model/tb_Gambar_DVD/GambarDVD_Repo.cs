c1363ec117de3ba868869e886365df3142bae6c7
reyhanfikri <yamato.reyhan123@gmail.com>
2018-05-02 11:53:38 +0700

    Nambahin controller dan fungsi2 gambar

--- tubesprovis/Model/tb_Gambar_DVD/GambarDVD_Repo.cs
+++ tubesprovis/Model/tb_Gambar_DVD/GambarDVD_Repo.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Drawing;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using Dapper;
@@ -41,11 +44,11 @@ namespace tubesprovis.Model.tb_Gambar_DVD
             }
         }
 
-        public List<GambarDVD_Class> getAllGenre()
+        public List<GambarDVD_Class> getAllCover()
         {
             try
             {
-                string query = "SELECT * FROM tb_genre";
+                string query = "SELECT * FROM tb_gambar_dvd";
 
                 OpenConnection();
                 List<GambarDVD_Class> hasil = myConn.Query<GambarDVD_Class>(query).ToList();
@@ -57,6 +60,62 @@ namespace tubesprovis.Model.tb_Gambar_DVD
             {
                 throw ex;
             }
+        }
+
+        public void insertNewCover(GambarDVD_Class cover)
+        {
+            int id_gambar = cover.Id_gambar;
+            int id_dvd = cover.Id_dvd;
+            string url_gambar = cover.Url_gambar;
+
+            string query = "INSERT INTO tb_gambar_dvd VALUES (" + id_gambar + ",'" + id_dvd + "','" + url_gambar + "');";
+
+            OpenConnection();
+            var hasil = myConn.Execute(query);
+            closeConnection();
+        }
+
+        public void coverDVD(GambarDVD_Class cover)
+        {
+            string sURL = cover.Url_gambar;
+
+            WebRequest req = WebRequest.Create(sURL); //jalur untuk mengakses url
+            WebResponse res = req.GetResponse();
+            Stream imgStream = res.GetResponseStream();
+
+           // Image Cover = Image.FromStream(imgStream); //generate image dari url
+            imgStream.Close();
+            var hasil = myConn.Execute(sURL);
+
+            closeConnection();
+        }
+
+        public void deleteCoverById(int Id)
+        {
+            try
+            {
+                string query = "DELETE FROM tb_gambar_dvd WHERE id = " + Id + ";";
+
+                OpenConnection();
+                var hasil = myConn.Execute(query);
+                closeConnection();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void updateCoverDVD(int id, string gambar_sampul)
+        {
+            string Cover = gambar_sampul;
+
+            string query = "UPDATE tb_gambar_dvd SET stock = " + Cover + " WHERE id_dvd = " + id + ";";
+
+            OpenConnection();
+            var hasil = myConn.Execute(query);
+            closeConnection();
         }
     }
 }
