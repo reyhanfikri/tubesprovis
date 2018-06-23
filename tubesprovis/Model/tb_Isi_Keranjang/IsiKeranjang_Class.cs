f1649918ee603899d69d1979e4383302a46750d0
AgungAnanta <agunganantawijaya@student.upi.edu>
2018-06-23 19:02:30 +0700

    + Query buat keranjang sesuai kostumer
    Menambah query untuk mendapatkan id  keranjang terbaru dari kustomer yang akan digunakan untuk query kedua

--- tubesprovis/Model/tb_Isi_Keranjang/IsiKeranjang_Class.cs
+++ tubesprovis/Model/tb_Isi_Keranjang/IsiKeranjang_Class.cs
@@ -8,8 +8,6 @@ namespace tubesprovis.Model.tb_Isi_Keranjang
     public class IsiKeranjang_Class
     {
         private int id_isi;
-        private int id_keranjang;
-        private int id_dvd;
         private String url_gambar;
         private String judul;
         private int qty;
@@ -22,8 +20,6 @@ namespace tubesprovis.Model.tb_Isi_Keranjang
         }
 
         public int Id_isi { get => id_isi; set => id_isi = value; }
-        public int Id_keranjang { get => id_keranjang; set => id_keranjang = value; }
-        public int Id_dvd { get => id_dvd; set => id_dvd = value; }
         public string Url_gambar { get => url_gambar; set => url_gambar = value; }
         public string Judul { get => judul; set => judul = value; }
         public int Qty { get => qty; set => qty = value; }
--- tubesprovis/Model/tb_Isi_Keranjang/IsiKeranjang_Repo.cs
+++ tubesprovis/Model/tb_Isi_Keranjang/IsiKeranjang_Repo.cs
@@ -46,10 +46,13 @@ namespace tubesprovis.Model.tb_Isi_Keranjang
         {
             try
             {
-                string query = "SELECT tb_gambar_dvd.url_gambar, tb_movie.judul , tb_detail_keranjang.qty, tb_detail_keranjang.harga_jual, tb_detail_keranjang.total_harga FROM tb_gambar_dvd INNER JOIN tb_detail_keranjang ON tb_gambar_dvd.id_dvd = tb_detail_keranjang.id_dvd INNER JOIN  tb_movie ON tb_gambar_dvd.id_movie = tb_movie.id_movie where tb_detail_keranjang.id_keranjang = "+id+" ORDER BY tb_detail_keranjang.id_detail";
+                string query = "SELECT id_keranjang FROM tb_keranjang where tb_keranjang.id_cust = "+id+" ORDER BY tb_detail_keranjang.id_detail DESC Limit 1;"
+                OpenConnection();
+                int id_keranjang = myConn.Query<Integer>()
+                query = "SELECT tb_gambar_dvd.url_gambar, tb_movie.judul , tb_detail_keranjang.qty, tb_detail_keranjang.harga_jual, tb_detail_keranjang.total_harga FROM tb_gambar_dvd INNER JOIN tb_detail_keranjang ON tb_gambar_dvd.id_dvd = tb_detail_keranjang.id_dvd INNER JOIN  tb_movie ON tb_gambar_dvd.id_movie = tb_movie.id_movie where tb_detail_keranjang.id_keranjang = "+id_keranjang+" ORDER BY tb_detail_keranjang.id_detail";
 
                 OpenConnection();
-                List<IsiKeranjang_Class> hasil = myConn.Query<IsiKeranjang_Class>(query).ToList();
+                List<IsiKeranjang_Class> hasil = myConn.Query(query).FirstOrDefault();
                 closeConnection();
 
                 return hasil;
