76031928b32696c2f6c718e271596f0e57987d75
AgungAnanta <agunganantawijaya@student.upi.edu>
2018-06-23 15:27:27 +0700

    Servis 3 - Isi Keranjang
    Buat Servis untuk Mendapatkan isi keranjang sesuai id costumer.
    dibuat class ,repo,controller untuk menggunakan servis isi detail keranjang

---
+++ tubesprovis/Model/tb_Isi_Keranjang/IsiKeranjang_Class.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tubesprovis.Model.tb_Isi_Keranjang
+{
+    public class IsiKeranjang_Class
+    {
+        private int id_isi;
+        private int id_keranjang;
+        private int id_dvd;
+        private String url_gambar;
+        private String judul;
+        private int qty;
+        private int harga_jual;
+        private int total_harga;
+
+        public IsiKeranjang_Class()
+        {
+
+        }
+
+        public int Id_isi { get => id_isi; set => id_isi = value; }
+        public int Id_keranjang { get => id_keranjang; set => id_keranjang = value; }
+        public int Id_dvd { get => id_dvd; set => id_dvd = value; }
+        public string Url_gambar { get => url_gambar; set => url_gambar = value; }
+        public string Judul { get => judul; set => judul = value; }
+        public int Qty { get => qty; set => qty = value; }
+        public int Harga_jual { get => harga_jual; set => harga_jual = value; }
+        public int Total_harga { get => total_harga; set => total_harga = value; }
+    }
+}
---
+++ tubesprovis/Model/tb_Isi_Keranjang/IsiKeranjang_Repo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace tubesprovis.Model.tb_Isi_Keranjang
+{
+    public class IsiKeranjang_Repo
+    {
+
+        private MySqlConnection myConn;
+
+        public IsiKeranjang_Repo()
+        {
+            string connectionString = "Server=localhost;Database=db_dvd;Username=root;Password=;SslMode=none";
+            myConn = new MySqlConnection(connectionString);
+        }
+
+        public void OpenConnection()
+        {
+            try
+            {
+                myConn.Open();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void closeConnection()
+        {
+            try
+            {
+                myConn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<IsiKeranjang_Class> getIsiKeranjang(int id)
+        {
+            try
+            {
+                string query = "SELECT tb_gambar_dvd.url_gambar, tb_movie.judul , tb_detail_keranjang.qty, tb_detail_keranjang.harga_jual, tb_detail_keranjang.total_harga FROM tb_gambar_dvd INNER JOIN tb_detail_keranjang ON tb_gambar_dvd.id_dvd = tb_detail_keranjang.id_dvd INNER JOIN  tb_movie ON tb_gambar_dvd.id_movie = tb_movie.id_movie where tb_detail_keranjang.id_keranjang = "+id+" ORDER BY tb_detail_keranjang.id_detail";
+
+                OpenConnection();
+                List<IsiKeranjang_Class> hasil = myConn.Query<IsiKeranjang_Class>(query).ToList();
+                closeConnection();
+
+                return hasil;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        
+
+    }
+}
