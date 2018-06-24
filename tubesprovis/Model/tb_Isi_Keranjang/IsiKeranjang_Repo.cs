c550d543d8a2b1df1d483a8158be9ac46c3d989d
AgungAnanta <agunganantawijaya@student.upi.edu>
2018-06-24 09:47:00 +0700

    Servis 3 latest working + tested
    servis sudah di tes dan berhasil

--- tubesprovis/Model/tb_Isi_Keranjang/IsiKeranjang_Repo.cs
+++ tubesprovis/Model/tb_Isi_Keranjang/IsiKeranjang_Repo.cs
@@ -46,13 +46,15 @@ namespace tubesprovis.Model.tb_Isi_Keranjang
         {
             try
             {
-                string query = "SELECT id_keranjang FROM tb_keranjang where tb_keranjang.id_cust = "+id+" ORDER BY tb_detail_keranjang.id_detail DESC Limit 1;"
+                string query = "SELECT id_keranjang FROM tb_keranjang where tb_keranjang.id_cust = "+id+" ORDER BY id_keranjang DESC Limit 1;";
                 OpenConnection();
-                int id_keranjang = myConn.Query<Integer>()
+                var hasilq = myConn.QuerySingle<int>(query);
+                int id_keranjang = (int) hasilq;
+                closeConnection();
                 query = "SELECT tb_gambar_dvd.url_gambar, tb_movie.judul , tb_detail_keranjang.qty, tb_detail_keranjang.harga_jual, tb_detail_keranjang.total_harga FROM tb_gambar_dvd INNER JOIN tb_detail_keranjang ON tb_gambar_dvd.id_dvd = tb_detail_keranjang.id_dvd INNER JOIN  tb_movie ON tb_gambar_dvd.id_movie = tb_movie.id_movie where tb_detail_keranjang.id_keranjang = "+id_keranjang+" ORDER BY tb_detail_keranjang.id_detail";
 
                 OpenConnection();
-                List<IsiKeranjang_Class> hasil = myConn.Query(query).FirstOrDefault();
+                List<IsiKeranjang_Class> hasil = myConn.Query<IsiKeranjang_Class>(query).ToList();
                 closeConnection();
 
                 return hasil;
